e5b3997436dbb92ea408a20dd63c0fcd7b17ea27
rydzol srydzol <rydzolr@gmail.com>
2023-01-28 22:18:29 +0100

    add FB game

---
+++ FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+     class FizzBuzz
+    {
+        public string CheckNumber(int val)
+        {
+            if (val % 3 == 0 && val % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (val%3==0)
+            {
+                return "Fizz";
+            }
+            if (val % 5 == 0)
+            {
+                return "Buzz";
+            }           
+            return val.ToString();
+        }
+    }
+}
---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            Console.WriteLine("Gra FizzBUZZ Podaj jakąś liczbę");
+            while (true)
+            {
+                Console.WriteLine("podaj Liczbę");
+                var number = InputNumber();
+                var wynik = fizzBuzz.CheckNumber(number);
+                Console.WriteLine(wynik);
+            }
+        }
+        private static int InputNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podaj Liczbę.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
